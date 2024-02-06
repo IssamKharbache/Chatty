c751d9b001e3851b7f90edaa03e78a5fe4886a89
lefline <issamkharbache2@gmail.com>
2024-02-07 00:31:43 +0100

    fixing deployment issues

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/api/users',usersRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname, "/frontend","dist","index.html"))
+res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
 })
 
 //run express server
