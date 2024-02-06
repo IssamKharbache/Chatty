339ca27905d88e7fb391b78775b2ba39c127def7
lefline <issamkharbache2@gmail.com>
2024-02-06 23:54:33 +0100

    fixing deployement issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContext, AuthContextProvider } from "./context/AuthContext.jsx";
-import { SocketContextProvider } from "./context/socketContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
