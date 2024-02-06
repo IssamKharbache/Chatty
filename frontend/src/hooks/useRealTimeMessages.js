b516b6befbb376cd2d4ebd76d5d97df9ecc36a35
lefline <issamkharbache2@gmail.com>
2024-02-06 23:58:56 +0100

    fixing deployement issues

--- frontend/src/hooks/useRealTimeMessages.js
+++ frontend/src/hooks/useRealTimeMessages.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useSocketContext } from "../context/socketContext"
+import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation";
 
 import notifSound from '../assets/sounds/notifsound.wav';
